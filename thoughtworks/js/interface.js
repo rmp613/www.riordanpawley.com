047030180e201f70172e770caa2b1227d59b5d80
Riordan Pawley <rmp613@gmail.com>
2017-06-25 14:33:17 +1000

    testing xhr7

--- thoughtworks/js/interface.js
+++ thoughtworks/js/interface.js
@@ -1,46 +1,13 @@
 
 var interface = (function(){
-   function request(method, url) {
-    return new Promise(function (resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.responseType = "blob";
-        xhr.open(method, url);
-        xhr.onload = resolve;
-        xhr.onerror = reject;
-        xhr.send();
-    });
-}
-
 // Do the request
    window.onload = function(){
-   request('GET', './test/input.txt')
-    .then(function (e) {
-        console.log(e.target.response);
-        console.log(e);
-    }, function (e) {
-        console.error("get request failed");
-    });
-
-
       var fileInput = document.getElementById("file-selector");
 
       fileInput.addEventListener('change', function(e){
          var file = fileInput.files[0];
-         var textType = /text.*/;
-
-         if(file.type.match(textType)) {
-            var reader = new FileReader();
-
-            reader.onload = function(e) {
-               clear();
-               write(noteProcessor.processNotes(reader.result));
-            }
-
-            reader.readAsText(file);
-         } else {
-            clear();
-            fileDiplayArea.innerText = "Please submit a .txt file";
-         }
+         loadFile(file);
+        
       });
    }
    /********** private methods **********/
@@ -63,13 +30,29 @@ var interface = (function(){
 
    /********** public methods **********/
    /*
-    * function that loads and processes a .txt file 
-    * takes [file]: a STRING like file:///C:/input.txt
+    * function that loads a .txt file 
+    * takes [file]: a blob object
     */
-   
+   function loadFile(file){
+      var textType = /text.*/;
+      if(file.type.match(textType)) {
+         var reader = new FileReader();
+
+         reader.onload = function(e) {
+            clear();
+            noteProcessor.processNotes(reader.result)
+         }
+
+         reader.readAsText(file);
+      } else {
+         clear();
+         fileDiplayArea.innerText = "Please submit a .txt file";
+      }
+   }
    /********** end public methods **********/
 
    var api = {
+      loadFile: loadFile
    }
 
    /* test code */
--- thoughtworks/test/interface-spec.js
+++ thoughtworks/test/interface-spec.js
@@ -1,19 +1,27 @@
-QUnit.test("interface exists", function(assert){
-  assert.notEqual(interface, undefined);
-})
+QUnit.test("interface exists", function (assert) {
+   assert.notEqual(interface, undefined);
+});
 
-QUnit.test("can write to results div", function(assert){
-  var xhr = new XMLHttpRequest();
-   xhr.open('GET', 'input.txt', true);
-
-   xhr.responseType = 'blob';
-
-   xhr.onload = function(e) {
-      if (this.status == 200) {
-         var blob = this.response;
-         console.log(blob);
-  assert.equal(blob, 1);
-      }
-   };
-
-})
+function request(method, url) {
+   return new Promise(function (resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = "blob";
+      xhr.open(method, url);
+      xhr.onload = resolve;
+      xhr.onerror = reject;
+      xhr.send();
+   });
+}
+QUnit.test("can write to results div", function (assert) {
+   var correctString = "pish tegj glob glob is 42<br>glob prok Silver is 68 Credits<br>glob prok Gold is 57800 Credits<br>glob prok Iron is 782 Credits<br>I have no idea what you are talking about <br>"
+   request("GET", "./test/input.txt")
+      .then(function(e){
+         interface.loadFile(e.target.response);
+         var responseElementText = document.getElementById("results").innerHTML;
+         console.log(responseElementText);
+         console.log(correctString);
+         assert.equal(responseElementText, correctString);
+      }, function(e) {
+         console.error("get request error");
+      });
+});
