7c514f9f5af430420e26a8e65f17b996365ea73c
Riordan Pawley <rmp613@gmail.com>
2017-06-25 14:42:52 +1000

    testing qunitasnc + xhr

--- thoughtworks/test/interface-spec.js
+++ thoughtworks/test/interface-spec.js
@@ -12,8 +12,8 @@ function request(method, url) {
       xhr.send();
    });
 }
-QUnit.asyncTest("can write to results div", function (assert) {
-   QUnit.stop();
+QUnit.test("can write to results div", function (assert) {
+   var done = assert.async();
    setTimeout(function(){
      
    var correctString = "pish tegj glob glob is 42<br>glob prok Silver is 68 Credits<br>glob prok Gold is 57800 Credits<br>glob prok Iron is 782 Credits<br>I have no idea what you are talking about <br>"
@@ -25,10 +25,11 @@ QUnit.asyncTest("can write to results div", function (assert) {
          console.log(correctString);
          
          assert.equal(responseElementText, correctString);
-         QUnit.start();
+         done();
       }, function(e) {
          console.error("get request error");
+         done();
       });
        
-   }, 1000);
+   });
 });
